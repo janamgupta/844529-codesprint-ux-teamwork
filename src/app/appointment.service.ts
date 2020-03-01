10b1ece515a20456879f52e793eaf46429270127
janamgupta <janamgupta98@gamil.com>
2020-03-02 00:23:03 +0530

    codesprint-ux

---
+++ src/app/appointment.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Appointment } from './appointment';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+
+  constructor(private httpClient : HttpClient) { }
+  placeAppointment(appointment:Appointment):Observable<any>{
+    console.log("service");
+    console.log(appointment);
+    return this.httpClient.post(`http://localhost:3000/appointment`,appointment);
+    }
+  getAppointments():Observable<any>{
+    return this.httpClient.get('http://localhost:3000/appointment');
+  }
+}
